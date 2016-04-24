13c097742e846bbd437699752a3ef223279b42a1
Byron Thanopoulos <byronth@gmail.com>
2016-04-24 19:33:24 +0100

    Adding images and MockData

---
+++ src/app/mockData/contacts-data.ts
@@ -0,0 +1,45 @@
+export let CONTACT_DATA = [
+          {
+            id: 0,
+            name: 'Byron Thanopoulos',
+            email: 'byronth@gmail.com',
+            phone: '+44 1606 000000',
+            birthday: '1971-03-03',
+            website: 'icode.net',
+            image: '/images/0.jpg',
+            address: {
+              street: 'icode av. 1',
+              zip: 'CW9 0PA',
+              city: 'Manchester',
+              country: 'UK'
+            }
+          },
+          {
+            id: 0,
+            name: 'Alex Meakin',
+            email: 'alex@gmail.com',
+            phone: '+44 1606 000000',
+            birthday: '1989-12-03',
+            website: 'icode.net',
+            image: '/images/2.jpg',
+            address: {
+              street: 'icode av. 1',
+              zip: 'CW9 0PA',
+              city: 'Manchester',
+              country: 'UK'
+            }
+          },{
+            id: 0,
+            name: 'Andy Lundy',
+            email: 'andy@gmail.com',
+            phone: '+44 1606 000000',
+            birthday: '1992-06-01',
+            website: 'icode.net',
+            image: '/images/2.jpg',
+            address: {
+              street: 'icode av. 1',
+              zip: 'CW9 0PA',
+              city: 'Manchester',
+              country: 'UK'
+            }
+          }]
